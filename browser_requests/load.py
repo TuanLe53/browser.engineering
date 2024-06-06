3a5d3c907478dddefb218ae118b330e5e5c44843
Tuan Le <leanhtuan030523@gmail.com>
2024-06-06 11:05:01 +0700

    Create show_source function and update load function

--- browser_requests/load.py
+++ browser_requests/load.py
@@ -1,4 +1,6 @@
 from url import URL
+import html
+from browser_requests.request_strategy import LocalFileStrategy, UrlDataStrategy, HttpStrategy, ViewSourceStrategy
 
 def show(body: str) -> None:
     in_tag: bool = False
@@ -10,5 +12,22 @@ def show(body: str) -> None:
         elif not in_tag:
             print(c, end="")
             
+def show_source(body: str) -> None:
+    decoded_body = html.unescape(body)
+    for c in decoded_body:
+        print(c, end="")
+
 def load(url: URL) -> None:
-    pass
+    if url.scheme in ["http", "https", "file"]:
+        if url.scheme == "http" or url.scheme == "https":
+            body = HttpStrategy.request(url)
+        elif url.scheme == "file":
+            body = LocalFileStrategy.request(url)
+        show(body)
+    
+    elif url.scheme in ["data", "view-source"]:
+        if url.scheme == "data":
+            body = UrlDataStrategy.request(url)
+        elif url.scheme == "view-source":
+            body = ViewSourceStrategy.request(url)
+        show_source(body)
