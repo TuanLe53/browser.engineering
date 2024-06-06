c06fa8a979a394f6b15ab5e11aa0b15fae547dca
Tuan Le <leanhtuan030523@gmail.com>
2024-06-06 11:04:21 +0700

    Update class to handle view-source scheme

--- url.py
+++ url.py
@@ -1,10 +1,10 @@
 from typing import List
 
-schemes: List[str] = ["http", "https", "file", "data"]
-
+schemes: List[str] = ["http", "https", "file", "data", "view-source"]
+#view-source:https://example.com/
 class URL:
     def __init__(self, url: str) -> None:
-        if url.startswith("data:"):
+        if url.startswith("data:") or url.startswith("view-source:"):
             self.scheme, url = url.split(":", 1)
         else:
             self.scheme, url = url.split("://", 1)
@@ -12,6 +12,21 @@ class URL:
         
         if self.scheme == "data":
             self.mime_type, self.data = url.split(",", 1)
+        elif self.scheme == "view-source":
+            self.source_scheme, url = url.split("://", 1)
+            if self.source_scheme == "http":
+                self.port: int = 80
+            elif self.source_scheme == "https":
+                self.port: int = 443
+            
+            if "/" not in url:
+                url = url + "/"
+            self.host, url = url.split("/", 1)
+            self.path = "/" + url
+            
+            if ":" in self.host:
+                self.host, port = self.host.split(":", 1)
+                self.port: int = int(port)
         else:
             if self.scheme == "http":
                 self.port: int = 80
