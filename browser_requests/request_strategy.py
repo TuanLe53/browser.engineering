d5aa1f2a0684d4eed31e2ad63dd2022cb00b5cc2
Tuan Le <leanhtuan030523@gmail.com>
2024-06-06 09:04:23 +0700

    Create LocalFileStrategy class

--- browser_requests/request_strategy.py
+++ browser_requests/request_strategy.py
@@ -6,3 +6,14 @@ class SchemeStrategy(ABC):
     def request(url: URL) -> str:
         pass
     
+class LocalFileStrategy(SchemeStrategy):
+    def request(url: URL) -> str:
+        #Get file extension
+        extension: str = url.path.split(".")[-1]
+        
+        if extension == "txt":
+            with open(url.path[1:], "r") as f:
+                content = f.read()
+                return content
+            
+        return "This file extension is not supported yet."
